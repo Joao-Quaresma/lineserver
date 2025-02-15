f5dcc77e49a76736097b4c12d97571fffd57ad62
JoaoQuaresma <joao.quaresma1991@gmail.com>
2025-02-15 10:15:54 +0000

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_15_100407) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_15_100433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
+  create_table "uploaded_files", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "size", null: false
+    t.integer "line_count", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
